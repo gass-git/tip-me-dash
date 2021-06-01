832fc8261ff58c36ed5e80f8df6c2a91f1a95b89
Gabriel Salinas <g.szada@gmail.com>
2021-06-01 18:22:22 +0200

    verified controller nows redirects to login

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -26,7 +26,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::DASHBOARD;
+    protected $redirectTo = '/login';
 
     /**
      * Create a new controller instance.

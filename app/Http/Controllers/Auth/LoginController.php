05d6f3b8daf0fe36fb76d51ccd8cf9b72779fdbf
Gabriel Salinas <g.szada@gmail.com>
2021-07-11 22:44:34 +0200

    Login improvement

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,9 +7,6 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Laravel\Socialite\Facades\Socialite;
 use Carbon\Carbon;
-use DB;
-use App\Http\Controllers\Auth;
-
 use App\User;
 use App\SocialProfile;
 use Illuminate\Http\Request;
@@ -82,48 +79,50 @@ class LoginController extends Controller
 
     public function handleProviderCallback(Request $request){
 
-        if($request->get('error')){    // If the user denise permission on google to provide login information, redirect to login page. 
+        // If the user denise permission on google to provide login information, redirect to login page.
+        if($request->get('error')){     
             return redirect()->route('login');
         }
 
-        $google_data = Socialite::driver('google')->user();   // Request data from Google
-        $registered_user = User::where('google_id', $google_data->getId())   // Search for user with Google id or email.
+        $google_data = Socialite::driver('google')->user();  // Request data from Google
+        
+        $registered_user = User::where('google_id', $google_data->getId())  // Search for user with Google id or email.
                                 ->orwhere('email',$google_data->email)
                                 ->first();
 
-        if(!$registered_user){    // Register user with google data if it's not registered.
+        /* ---- Create user with requested data if it's not registered ---- */
+        if(!$registered_user){    
 
-            $data = array();
-            $data['google_id'] = $google_data->getId();
-            $data['email'] = $google_data->getEmail();
-            $data['avatar_url'] = 'https://tipmedash.com/images/avatar-default-1.jpg';
-            $data['points'] = 10;
-            $data['username_color'] = 'rgb(255,255,255)';
-            $data['email_verified_at'] = Carbon::now();
-            $data['created_at'] = Carbon::now();
-            $data['updated_at'] = Carbon::now();
-
-            DB::table('users')->insert($data);
+            User::create([ 
+                'google_id' => $google_data->getId(),
+                'email' => $google_data->getEmail(),
+                'avatar_url' => "https://tipmedash.com/images/avatar-default-1.jpg",
+                'points' => 10,
+                'username_color' => "rgb(255,255,255)",
+                'email_verified_at' => Carbon::now(),
+                'created_at' => Carbon::now()
+            ]);
 
             $registered_user = User::where('google_id', $google_data->getId())->first();  // Find user after registering.
         }
+        /* ---------------------------------------------------------------- */
 
-            /**@abtract
-             * 
-             * Conditions in case the user changes email and logs in with Google:
-             * 1) The user is registered with his google email but without the google_id assigned.
-             * 2) The user is registered with google email or id, but his email is not verified.
-             * 
-             */
-            if($registered_user->google_id == null){
-                $registered_user->google_id = $google_data->getId();
-                $registered_user->save();
-            }
-
-            if($registered_user->email_verified_at == null){
-                $registered_user->email_verified_at = Carbon::now();
-                $registered_user->save();
-            }
+        /**@abtract
+         * 
+         * Conditions in case the user changes email and logs in with Google:
+         * 1) The user is registered with his google email but without the google_id assigned.
+         * 2) The user is registered with google email or id, but his email is not verified.
+         * 
+         */
+        if($registered_user->google_id == null){
+            $registered_user->google_id = $google_data->getId();
+            $registered_user->save();
+        }
+        
+        if($registered_user->email_verified_at == null){
+            $registered_user->email_verified_at = Carbon::now();
+            $registered_user->save();
+        }
 
         auth()->login($registered_user);
         return redirect()->route('dashboard');

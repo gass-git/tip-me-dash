7dfd6cd545dfb8925da158de112209f7776d28df
Gabriel Salinas <g.szada@gmail.com>
2021-07-14 11:37:05 +0200

    Font size changes on welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
         <!-- bootstrap css -->
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         <!-- main css -->
-        <link rel="stylesheet" href="{{ asset('css/main.css?60') }}">
+        <link rel="stylesheet" href="{{ asset('css/main.css?61') }}">
         <!-- font awesome css -->
         <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
         

0b8df4b5fcc72118f2997d2b5e51e8cedc1bc62b
Gabriel Salinas <g.szada@gmail.com>
2021-07-17 22:18:52 +0200

    Fixed dashboard bug related to number of Tips Received for new user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,6 +70,8 @@ class RegisterController extends Controller
             'username' => $data['username'],
             'email' => $data['email'],
             'points' => 10,
+            'sent' => 0,
+            'received' => 0,
             'password' => Hash::make($data['password']),
             'username_color' => 'rgb(255,255,255)'
         ]);

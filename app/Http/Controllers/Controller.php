642dd919546776524761ccbf4d8fb0c414915a6d
Gabriel Salinas <g.szada@gmail.com>
2021-07-21 20:27:21 +0200

    Fix: error on recently registered list for users without a username

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -15,7 +15,7 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function show_welcome(){
-        $newcomers = User::orderBy('id','DESC')->get();
+        $newcomers = User::whereNotNull('username')->orderBy('id','DESC')->get();
         $ranks = User::orderBy('points','DESC')->get();
         return view('welcome',compact('newcomers','ranks'));
     }

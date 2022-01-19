bb098dba5937d671c94bf14ea1c8602294ed64ea
Gabriel Salinas <g.szada@gmail.com>
2022-01-19 20:55:41 +0100

    Scrollbar style improved

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
         <!-- bootstrap css -->
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         <!-- main css -->
-        <link rel="stylesheet" href="{{ asset('css/main.css?2') }}">
+        <link rel="stylesheet" href="{{ asset('css/main.css?3') }}">
         <!-- font awesome css -->
         <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
         
